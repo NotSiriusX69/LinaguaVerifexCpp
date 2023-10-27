4789035da81208ae5bfabc531a38b9ae96732367
HyperJAK <james.jakthesamething@gmail.com>
2023-10-27 18:24:25 +0300

    Added new transition struct properties

--- Automat.h
+++ Automat.h
@@ -13,16 +13,30 @@ const std::string path = "data/automat.json";
 
 // Transition structure 
 struct Transition {
-    char origin;
-    char label;
-    char target;
+	//origin state, target state, and current regex block (ex: [0-9]^+  snf its regexToken would be: NUM)
+	char origin;
+	std::string regexBlock;
+	char target;
+
+
+	std::string regexToken;
+
+	//user can choose to write it or not, and if he writes it, then he can infinitely continue
+	bool starRepetition = false;
+
+	//user has to write it, and when he writes it, then he can infinitely continue
+	bool plusRepetition = false;
+
+	//user can write it once or not at all
+	bool emptyRepetition = false;
+
 };
 
 // Automat structure
 struct Automat {
-    char alphabet[nbSymbols];
-    int states[nbStates];
-    char q0; //Initial state
-    std::vector<Transition> delta;
-    char statesTerminal[nbStatesTerminal];
+	char alphabet[nbSymbols];
+	int states[nbStates];
+	char q0; //Initial state
+	std::vector<Transition> delta;
+	char statesTerminal[nbStatesTerminal];
 };
